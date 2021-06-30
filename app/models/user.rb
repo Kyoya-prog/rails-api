0114831ffe1e95e4193493f7f9d4817875af1f4f
kyoya <kyoyaprog@gmail.com>
2021-06-30 23:16:28 +0900

    emailを一意なものにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :email,presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :email,uniqueness: {case_sensitive: true}
 end
