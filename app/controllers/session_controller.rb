16bee8afc5923262d8311dc0aaa432a77e35f281
kyoya <kyoyaprog@gmail.com>
2021-07-01 14:34:28 +0900

    ユーザーがログインできたらセッションを作成し、トークンを渡すようにする

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,12 @@
 class SessionController < ApplicationController
+  def create
+    user = User.find_by(email:params[:email])
+    token = ""
+    status = :unauthorized
+    if user && user.authenticate(params[:password])
+      token = Session.create(user)
+      status = :created
+    end
+    render json: { token: token },status: status
+  end
 end
