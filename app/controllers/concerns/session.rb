75b9fd3a589b890a3f9bed759a4328e5be716039
kyoya <kyoyaprog@gmail.com>
2021-07-01 15:13:28 +0900

    ポストリクエストが送信できるようにする

--- app/controllers/concerns/session.rb
+++ app/controllers/concerns/session.rb
@@ -1,10 +1,11 @@
 module Session
   def self.create(user)
     token = SecureRandom.hex(64)
-    Redis.mapped_hmset(
+    REDIS.mapped_hmset(
       token,
       'user_id' => user.id
-    )
-    Redis.expire(token,Settings.session.timeout)
+      )
+    REDIS.expire(token,Settings.session.timeout)
+    return token
   end
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,6 +1,6 @@
 class SessionController < ApplicationController
   def create
-    user = User.find_by(email:params[:email])
+    user = User.find_by(email: params[:email])
     token = ""
     status = :unauthorized
     if user && user.authenticate(params[:password])
