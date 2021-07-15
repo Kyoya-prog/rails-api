bdb1de4af29e48ca95f871ae086cd254c4adfc0b
kyoya <kyoyaprog@gmail.com>
2021-07-15 15:58:39 +0900

    ドメインをapplication.rbに追加

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,7 @@ module ApiPractice
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.hosts << ".kyoyaprog-saving.com"
   end
 end
