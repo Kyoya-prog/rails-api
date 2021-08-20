6010c566f76f7258172db7440920b5f5358621f0
kyoya <kyoyaprog@gmail.com>
2021-08-20 13:55:30 +0900

    descriptionにかけていたバリデーションをmemoに変更

--- app/models/patience.rb
+++ app/models/patience.rb
@@ -1,5 +1,5 @@
 class Patience < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :description,length: { maximum: 140 }
+  validates :memo,length: { maximum: 140 }
 end
