4d0cddaba3abb8b89f72947dd87248661bb6d72b
kyoya <kyoyaprog@gmail.com>
2021-09-08 19:18:38 +0900

    Userの作成処理にemailとpassswordを使用しないようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(email:params[:email].downcase,password:params[:password])
+    @user = User.new()
     @user.save!
     token = Session.create(@user)
     render json: {token:token},status: :ok
